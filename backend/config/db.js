e91a124531e7b0979443bdd66de0e21bfb67834f
ntoiti <fidesluckia36@gmail.com>
2025-09-19 15:01:35 +0300

    coonected to mongoB

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,12 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 export const connectDB=async()=>{
-    
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("MongoDB connected succesfully")
+    }catch(error){
+        console.error("Error connecting to MongoDB",error.message)
+    }
 }
+connectDB()
 
