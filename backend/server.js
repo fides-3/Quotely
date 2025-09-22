d569d21db7e0644a92e2531b5233821b7944597e
ntoiti <fidesluckia36@gmail.com>
2025-09-22 21:24:11 +0300

    corrected code

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,45 @@
-const express=require('express');
+import express from  'express';
 import cors from 'cors';
 import path from 'path';
-import {corsOptions} from ".config/corsOptions.js";
+import {corsOptions} from "./config/corsOptions.js";
 import {connectDB} from './config/db.js';
 import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser';
-import authRoute from './routes/authRouts.js';
+import authRoute from './routes/authRoutes.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app=express();
-const PORT=process.env.PORT ||5000;
+const PORT=process.env.PORT ||3000;
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
+app.use(express.json());
+
+// static files
+app.use(express.static(path.join(__dirname,'public')))
+
+
+app.use("/auth",authRoute);
+
+app.get("/",(req,res)=>{
+    res.send('API is running...');
+    console.log("works");
+});
+
+const startServer=async()=>{
+    try{
+        await connectDB();
+        app.listen(PORT,()=>{
+            console.log(`server running on port :${PORT}`)
+        });
+    }catch(error){
+        console.error("Failed to connect to database:",error);
+        process.exit(1);
+    }
+};
+startServer()
+
