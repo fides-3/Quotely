6cdc099dece4f25416f767533b202b2fcb724dd5
ntoiti <fidesluckia36@gmail.com>
2025-09-26 12:27:48 +0300

    added code

---
+++ frontend/app/providers.tsx
@@ -0,0 +1,5 @@
+import { ThemeProvider } from "next-themes";
+
+export function Providers({children}:{children:React.ReactNode}){
+    return <ThemeProvider attribute ='class' defaultTheme="light" enableSystem>{children}</ThemeProvider>
+}
