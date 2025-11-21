ae188a538ad98c0281f13cf64b2398c9c81b0e2d
ntoiti <fidesluckia36@gmail.com>
2025-11-21 16:58:16 +0300

    added routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 const router = express.Router();
 import * as authController from '../controllers/authController.js';
-// import {verifyToken} from '../middleware/verifyToken.js'
+import {verifyToken} from '../middleware/verifyToken.js'
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.post('/forgotPassword', authController.forgotPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.post('/forgot-password', authController.forgotPassword);
+router.post('/reset-password', authController.resetPassword);
+router.get('/refresh', authController.refreshToken);
+router.get('/profile', verifyToken, authController.getprofile);
+router.put('/profile', verifyToken, authController.updateProfile);
+router.delete('/profile', verifyToken, authController.deleteProfile);
+router.post('/logout', authController.logout);
 
 export default router;
