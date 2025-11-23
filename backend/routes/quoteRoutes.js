2700ac76e437069f83636d61fcd76a56a15ead12
ntoiti <fidesluckia36@gmail.com>
2025-11-23 14:03:12 +0300

    added middleware

--- backend/routes/quoteRoutes.js
+++ backend/routes/quoteRoutes.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import * as quoteContoller from '../controllers/quoteController.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router=express.Router();
-router.post('/createQuote',quoteContoller.createQuote);
+router.post('/createQuote', verifyToken, quoteContoller.createQuote);
+router.get('/my-quotes', verifyToken, quoteContoller.getUserQuotes);
+router.get('/user/:userId', quoteContoller.getQuotesByUserId);
+router.post('/:quoteId/like', verifyToken, quoteContoller.toggleLikeQuote);
+router.post('/:quoteId/comment', verifyToken, quoteContoller.addComment);
+router.get('/total-likes', verifyToken, quoteContoller.getUserTotalLikes);
 
 export default router;
